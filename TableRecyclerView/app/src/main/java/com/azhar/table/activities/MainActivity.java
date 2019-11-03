e096cbe90301adb0c81ec5c4ac1870ec7d020c51
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2019-11-03 20:55:44 +0700

    Add files via upload

---
+++ TableRecyclerView/app/src/main/java/com/azhar/table/activities/MainActivity.java
@@ -0,0 +1,49 @@
+package com.azhar.table.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.azhar.table.R;
+import com.azhar.table.adapter.TableViewAdapter;
+import com.azhar.table.model.MovieModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerViewDeliveryProductList);
+
+        TableViewAdapter adapter = new TableViewAdapter(getMovieList());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<MovieModel> getMovieList() {
+        List<MovieModel> movieList = new ArrayList<>();
+
+        movieList.add(new MovieModel(1, "Pirates of the Caribbean: On Stranger Tides", 2011, 378));
+        movieList.add(new MovieModel(2, "Avengers: Age of Ultron", 2015, 365));
+        movieList.add(new MovieModel(3, "Avengers: Infinity War", 2018, 316));
+        movieList.add(new MovieModel(4, "Pirates of the Caribbean: At World's End", 2007, 300));
+        movieList.add(new MovieModel(5, "Justice League", 2017, 300));
+        movieList.add(new MovieModel(6, "Solo: A Star Wars Story", 2018, 275));
+        movieList.add(new MovieModel(7, "John Carter", 2012, 264));
+        movieList.add(new MovieModel(8, "Batman v Superman: Dawn of Justice", 2016, 263));
+        movieList.add(new MovieModel(9, "Star Wars: The Last Jedi", 2017, 263));
+        movieList.add(new MovieModel(10, "Tangled", 2010, 260));
+
+        return movieList;
+    }
+}
---
+++ TableRecyclerView/app/src/main/java/com/azhar/table/adapter/TableViewAdapter.java
@@ -0,0 +1,87 @@
+package com.azhar.table.adapter;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.azhar.table.R;
+import com.azhar.table.model.MovieModel;
+
+import java.util.List;
+
+public class TableViewAdapter extends RecyclerView.Adapter {
+
+    private List<MovieModel> movieList;
+
+    public TableViewAdapter(List<MovieModel> movieList) {
+        this.movieList = movieList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.
+                from(parent.getContext()).
+                inflate(R.layout.table_list_item, parent, false);
+
+        return new RowViewHolder(itemView);
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        RowViewHolder rowViewHolder = (RowViewHolder) holder;
+
+        int rowPos = rowViewHolder.getAdapterPosition();
+
+        if (rowPos == 0) {
+
+            rowViewHolder.txtRank.setBackgroundResource(R.drawable.table_header_cell_bg);
+            rowViewHolder.txtMovieName.setBackgroundResource(R.drawable.table_header_cell_bg);
+            rowViewHolder.txtYear.setBackgroundResource(R.drawable.table_header_cell_bg);
+            rowViewHolder.txtCost.setBackgroundResource(R.drawable.table_header_cell_bg);
+
+            rowViewHolder.txtRank.setText("Rank");
+            rowViewHolder.txtMovieName.setText("Name");
+            rowViewHolder.txtYear.setText("Year");
+            rowViewHolder.txtCost.setText("Budget");
+        } else {
+            MovieModel modal = movieList.get(rowPos - 1);
+
+            rowViewHolder.txtRank.setBackgroundResource(R.drawable.table_content_cell_bg);
+            rowViewHolder.txtMovieName.setBackgroundResource(R.drawable.table_content_cell_bg);
+            rowViewHolder.txtYear.setBackgroundResource(R.drawable.table_content_cell_bg);
+            rowViewHolder.txtCost.setBackgroundResource(R.drawable.table_content_cell_bg);
+
+            rowViewHolder.txtRank.setText(modal.getRank() + "");
+            rowViewHolder.txtMovieName.setText(modal.getMovieName());
+            rowViewHolder.txtYear.setText(modal.getYear() + "");
+            rowViewHolder.txtCost.setText(modal.getBudgetInMillions() + "");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieList.size() + 1;
+    }
+
+    public class RowViewHolder extends RecyclerView.ViewHolder {
+        TextView txtRank;
+        TextView txtMovieName;
+        TextView txtYear;
+        TextView txtCost;
+
+        RowViewHolder(View itemView) {
+            super(itemView);
+            txtRank = itemView.findViewById(R.id.txtRank);
+            txtMovieName = itemView.findViewById(R.id.txtMovieName);
+            txtYear = itemView.findViewById(R.id.txtYear);
+            txtCost = itemView.findViewById(R.id.txtCost);
+        }
+    }
+}
